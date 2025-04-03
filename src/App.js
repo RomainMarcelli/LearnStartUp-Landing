beaef1546c15a597f71231d64228ddbeb17c777e
Romain MARCELLI <r.marcelli31@gmail.com>
2025-04-03 11:49:54 +0200

    partie téléphone fait

--- src/App.js
+++ src/App.js
@@ -80,6 +80,26 @@ function App() {
           </div>
         </div>
       </section>
+
+      {/* Section Présentation App */}
+      {/* Section Présentation App */}
+      <section className="bg-white py-24">
+        <div className="max-w-7xl mx-auto px-8 grid md:grid-cols-2 gap-16 items-center">
+          <div>
+            <h2 className="text-3xl font-semibold mb-6">Qu’est-ce que HoopSphère?</h2>
+            <p className="text-gray-800 text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet massa condimentum, tempor odio vel,
+              tristique turpis. Quisque blandit libero sit amet justo facilisis, sed porta metus consectetur. Nulla a diam
+              sem. Fusce rutrum ipsum sem, eu aliquet ex elementum ac. Nullam nibh ante, condimentum eget rhoncus id,
+              consequat eu neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+              himenaeos
+            </p>
+          </div>
+          <div className="flex justify-center gap-6">
+          <img src="/img/iphone.png" alt="Phone 1" className="w-[33rem]" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
